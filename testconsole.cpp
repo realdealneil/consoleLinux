2c1a6b447228e29969dd98f3399ce53427dc5a24
Neil Johnson <neil@example.com>
2016-01-09 15:39:37 -0500

    Added time and some color

--- testconsole.cpp
+++ testconsole.cpp
@@ -1,29 +1,39 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 #include "consoleLinux.h"
+
+/**
+ *  double ms_time(void): returns the current time as a double floating point
+ *  time since an arbitrary initial time.  We use the monotonic timer which can't
+ *  go backwards.
+ */
+double ms_time(void)
+{
+    struct timespec now_timespec;
+    clock_gettime(CLOCK_MONOTONIC, &now_timespec);
+    return ((double)now_timespec.tv_sec)*1000.0 + ((double)now_timespec.tv_nsec)*1.0e-6;
+}
 
 int main(void)
 {
+	double start_time = ms_time();
 	consoleLinux con;
 	con.clearScreen();
-	con.setCursorPosition(1, 1);
-	printf("first line here");
 	con.setCursorPosition(2,10);
 	
-	con.setColor(COL_RED);
-	printf("second line here");
-	con.setCursorPosition(30,1);
-	con.setColor(COL_NORMAL);
-	printf("last line here");
-	con.setCursorPosition(12,1);
-	
 	int count=0;
-	while(count++ < 5) {
+	while(count++ < 50) {
 		con.clearScreen();
+		
+		con.setCursorPosition(1, 1);
+		printf("first line here");
 		con.setCursorPosition(2,1);		
-		printf("Replaced second line %d times", count);		
+		printf("Updated " ANSI_COLOR_BLUE "%d" ANSI_COLOR_NORMAL " times", count);		\
+		con.setCursorPosition(3,1);
+		printf("Time since start: " ANSI_COLOR_RED "%07.2lf" ANSI_COLOR_NORMAL, ms_time() - start_time);
 		con.updateScreen();
-		usleep(1000000);
+		usleep(100000);
 	}	
 
 	con.setCursorPosition(31,1);
